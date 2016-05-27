90851aef5174a9a79941e5fe39c5ce79276a3de8
Wesley Klop <wesley19097@gmail.com>
2016-05-27 23:56:29 +0200

    Finalize skills section, add normalize.css locally, start portfolio styling, improve pagespeed insights

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,19 @@
 /**
  * Created by wesley on 5/25/16.
  */
+
+((d, w) => {
+    // Create home anchor depending on viewport
+    let home = d.createElement('div'),
+        anchor;
+    home.id = 'home';
+    if (w.innerWidth > 1023) {
+        anchor = d.querySelector('main');
+
+    } else {
+        anchor = d.querySelector('body');
+    }
+    anchor.insertBefore(home, anchor.firstElementChild)
+
+    
+})(document, window);
